b5258f59a9396b5badf416a7e103cc075456e849
Francisco Javier Galián Piñero <136383537+javigalii@users.noreply.github.com>
2025-12-14 17:45:52 +0100

    smooth transition

--- app.js
+++ app.js
@@ -37,3 +37,22 @@ if (
     themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
   }
 }
+
+// Lógica del Menú Hamburguesa
+const menuToggle = document.querySelector('#mobile-menu');
+const navMenu = document.querySelector('.navbar nav');
+const navLinks = document.querySelectorAll('.navbar nav ul li a');
+
+// Abrir / Cerrar menú
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('is-active');
+    navMenu.classList.toggle('active');
+});
+
+// Cerrar menú al hacer click en un enlace
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuToggle.classList.remove('is-active');
+        navMenu.classList.remove('active');
+    });
+});
